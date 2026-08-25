dce8bf0f4f9b5f12892dc07c367fa87eb157dfd2
lkwarwick <lucaswarwick02@gmail.com>
2026-08-25 16:27:39 +0100

    Set up Fabric 26.2 mod

---
+++ src/client/java/lkwarwick/deterministicenchanting/client/DeterministicEnchantingClient.java
@@ -0,0 +1,10 @@
+package lkwarwick.deterministicenchanting.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeterministicEnchantingClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
